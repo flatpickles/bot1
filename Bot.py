ec7a26c8d9b10abff888886553221aed94973fef
Matt Nichols <matt@mattnichols.net>
2013-10-28 23:46:48 -0700

    Adding mechanics for tweeting and retweeting, hopefully smoothing everything else out a little too.

--- Bot.py
+++ Bot.py
@@ -11,9 +11,25 @@ class Bot:
   ### external methods ###
 
   def __init__(self):
-    self._log("spinning up")
-    self.toFollowQueue = multiprocessing.Queue()
-    self.setInterestedTopics(config.DEFAULT_INTERESTS)
+    self._name = None
+    self._log("spinning up a new bot")
+    self._toFollowQueue = multiprocessing.Queue() # contains user IDs
+
+    # configure this instance with defaults (these fields can be configured on a bot-by-bot basis)
+    self.location = config.LOCATION
+    self.language = config.LANGUAGE
+    self.interests = config.INTERESTS
+    self.useTrendsForFollows = config.USE_TRENDS_FOR_FOLLOWS
+    self.useTrendsForTweets = config.USE_TRENDS_FOR_TWEETS
+    self.useTrendsForRetweets = config.USE_TRENDS_FOR_RETWEETS
+    self.useTrendsForFaves = config.USE_TRENDS_FOR_FAVES
+    self.searchTypeForFollows = config.SEARCH_TYPE_FOR_FOLLOWS
+    self.searchTypeForTweets = config.SEARCH_TYPE_FOR_TWEETS
+    self.searchTypeForRetweets = config.SEARCH_TYPE_FOR_RETWEETS
+    self.searchTypeForFaves = config.SEARCH_TYPE_FOR_FAVES
+    self.interestDist = config.INTEREST_DIST
+    self.faveDist = config.FAVE_DIST
+    self.tweetDist = config.TWEET_DIST
 
   # initiates the api reference with keys & secrets in a file named credsPath
   def authWithCredsFile(self, credsPath):
@@ -24,29 +40,73 @@ class Bot:
       auth.set_access_token(credsList[2], credsList[3])
       self.api = tweepy.API(auth)
       f.close()
+      me = self.api.me()
+      self._name = me.screen_name
+      self._id = me.id
       self._log("authentication succeeded")
     except Exception as e:
       self._log("authentication failed")
       traceback.print_exc()
 
-  def setInterestedTopics(self, topicsArray):
-    self.topicsArray = topicsArray
-
-  def startLoops(self):
+  def start(self):
     # start a loop to follow a new follower at each interval
     self.followBackProcess = multiprocessing.Process(target=self._followLoop, args=(config.FOLLOW_INTERVAL,))
-    self.followBackProcess.start()
-    self._log("follow-back process started")
+    self.followBackProcess.start()\
     # start a loop to find new users to follow periodically
     self.followSearchProcess = multiprocessing.Process(target=self._followSearchLoop, args=(config.FOLLOW_SEARCH_INTERVAL,))
     self.followSearchProcess.start()
-    self._log("follow search process started")
+    # start a loop to tweet on occasion
+    self.tweetProcess = multiprocessing.Process(target=self._tweetLoop, args=(config.TWEET_INTERVAL,config.TWEET_PERTURBATION,))
+    self.tweetProcess.start()
+
+    self._log("all processes started")
 
   # kill all processes, etc
-  def teardown(self):
+  def stop(self):
     self.followBackProcess.terminate()
     self.followSearchProcess.terminate()
-    self._log("termination succeeded")
+    self.tweetProcess.terminate()
+    self._log("all processes stopped")
+
+  ### tweet or retweet occasionally ###
+
+  def _tweetLoop(self, interval, perturbation):
+    while True:
+      try:
+        r = random.random()
+        if r < self.tweetDist[0]:
+          # retweet a tweet from someone followed by this bot
+          self._retweetFriendTweet()
+        elif r < self.tweetDist[0] + self.tweetDist[1]:
+          # retweet a tweet from a search
+          self._retweetSearchTweet()
+        else:
+          # tweet the text of a tweet from a search
+          self._tweetSearchTweet()
+      except Exception as e:
+        self._log("encountered an exception in tweet loop")
+        traceback.print_exc()
+      toWait = interval + (random.random() * 2 * perturbation - perturbation)
+      time.sleep(int(toWait))
+
+  def _retweetFriendTweet(self):
+    tweet = self._getFriendTweet()
+    self.api.retweet(id=tweet.id)
+    self._log("retweeted @%s/%s (from friend)" % (tweet.user.screen_name, tweet.id_str))
+
+  def _retweetSearchTweet(self):
+    tweet = self._getTopicalTweet(self.useTrendsForRetweets, self.searchTypeForRetweets)
+    self.api.retweet(id=tweet.id)
+    self._log("retweeted @%s/%s (from search)" % (tweet.user.screen_name, tweet.id_str))
+
+  def _tweetSearchTweet(self):
+    tweet = self._getTopicalTweet(self.useTrendsForTweets, self.searchTypeForTweets)
+    # never tweet the text of RTs
+    if 'RT @' in tweet.text:
+      self._tweetSearchTweet()
+    else:
+      self.api.update_status(status=tweet.text)
+      self._log("reproduced @%s/%s (from search)" % (tweet.user.screen_name, tweet.id_str))
 
   ### follow search ###
 
@@ -61,7 +121,7 @@ class Bot:
 
   def _findUserToFollow(self):
     # search tweets instead of users for more dynamic results
-    tweet = self._getTopicalTweet()
+    tweet = self._getTopicalTweet(self.useTrendsForFollows, self.searchTypeForFollows)
     willFollow = self._queueFollow(tweet.user)
     if not willFollow:
       # find another user if this one wasn't queued
@@ -91,7 +151,7 @@ class Bot:
     try:
       # check if already followed, in case there are duplicate entries in the queue
       # more straightforward to also keep a set of objects in the queue, but not as safe across processes
-      earliestFollowId = self.toFollowQueue.get(block=False)
+      earliestFollowId = self._toFollowQueue.get(block=False)
       user = self.api.get_user(id=earliestFollowId)
       if not user.following:
         self.api.create_friendship(user_id=earliestFollowId)
@@ -106,16 +166,40 @@ class Bot:
 
   def _queueFollow(self, user):
     if not user.following:
-      self.toFollowQueue.put(user.id)
+      self._toFollowQueue.put(user.id)
       return True
     return False
 
-  def _getTopicalTweet(self):
-    currTopic = random.choice(self.topicsArray)
-    results = self.api.search(q=currTopic)
-    return random.choice(results)
+  def _getTopicalTweet(self, useTrends, searchType):
+    options = self.interests
+    r = random.random()
+    if useTrends and r >= self.interestDist[0]:
+      options = [trend['name'] for trend in self.api.trends_place(id=self.location)[0]['trends']]
+    currTopic = random.choice(options)
+    options = self.api.search(q=currTopic, search_type=searchType)
+    selection = self._langAppropriateTweet(options)
+    return selection if selection != None else self._getTopicalTweet(useTrends, searchType)
+
+  def _getFriendTweet(self):
+    options = self.api.home_timeline(exclude_replies=True)
+    selection = self._langAppropriateTweet(options)
+    return selection if selection != None else self._getFriendTweet()
+
+  def _langAppropriateTweet(self, tweets):
+    selection = random.choice(tweets)
+    maxAttempts = 20
+    # make sure this isn't already tweeted and is in desired language
+    while (selection.lang != self.language or selection.user.id == self._id) and maxAttempts > 0:
+      selection = random.choice(tweets)
+      maxAttempts -= 1
+    return selection if maxAttempts > 0 else None
 
   # print a message to stdout with a timestamp
   def _log(self, message):
-    timestamp = datetime.datetime.now().strftime("[%m-%d-%Y %H:%M]")
-    print "%s %s" % (timestamp, message)
+    timestamp = datetime.datetime.now().strftime("%m-%d-%Y %H:%M")
+    if not self._name:
+      print "[%s] %s" % (timestamp, message)
+    else:
+      print "[%s ~ @%s] %s" % (timestamp, self._name.lower(), message)
+
+
--- config.py
+++ config.py
@@ -1,6 +1,22 @@
 ### time between actions - some imposed by rate limiting ###
-FOLLOW_INTERVAL = 60
-FOLLOW_SEARCH_INTERVAL = 1800
+FOLLOW_INTERVAL = 60 # one minute
+FAVE_INTERVAL = 10 * 60 # 10 minutes
+FOLLOW_SEARCH_INTERVAL = 30 * 60 # 30 minutes
+TWEET_INTERVAL = 3 * 60 * 60 # 3 hours
+TWEET_PERTURBATION = 60 * 60 # if you will; 1 hour
 
-### defaults - configurable ###
-DEFAULT_INTERESTS = ["follow", "follow back", "retweet", "teamfollowback", "bieber"]
+### defaults - configurable as properties on an instance of Bot ###
+LOCATION = 23424977 # place ID - USA
+LANGUAGE = 'en'
+INTERESTS = ["follow", "follow back", "retweet", "teamfollowback"]
+INTEREST_DIST = [.6, .4] # [interests array, current top trends]
+FAVE_DIST = [.3, .7] # [fave from following, fave from search]
+TWEET_DIST = [.15, .5, .35] # [rt following, rt from search, borrow from search]
+USE_TRENDS_FOR_FOLLOWS = False
+USE_TRENDS_FOR_TWEETS = True
+USE_TRENDS_FOR_RETWEETS = True
+USE_TRENDS_FOR_FAVES = True
+SEARCH_TYPE_FOR_FOLLOWS = 'recent'
+SEARCH_TYPE_FOR_TWEETS = 'recent'
+SEARCH_TYPE_FOR_RETWEETS = 'mixed'
+SEARCH_TYPE_FOR_FAVES = 'mixed'
